fc72f86e2b42945a8296e282b88d101d2212ae36
Gina Henderson <gina@ginahenderson.me>
2022-08-25 22:19:12 -0500

    Create play and pause button

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 var balloon = document.getElementsByClassName('balloon');
 var balloonContainer = document.getElementsByClassName('balloon-container');
 var wrapper = document.getElementById('wrapper');
+const balloonButton = document.querySelector('.balloon-button');
+const title = document.querySelector('.title');
+
+let missed = 0;
+let interval;
 
 function releaseBalloon() {
   var colors = ['173, 81%, 83%', '173, 33%, 95%', '342, 67%, 90%', '330, 70%, 83%', '173, 80%, 72%', '174, 33%, 68%', '342, 67%, 76%', '330, 70%, 76%', '43, 84%, 80%'];
   var speed = ['9s', '10s', '11s', '12s'];
   let newBalloon = document.createElement('div');
   newBalloon.classList.add('balloon');
-  setTimeout(function() {
-  newBalloon.classList.add('balloon-rise');
+  setTimeout(function () {
+    newBalloon.classList.add('balloon-rise');
   }, 100);
   newBalloon.setAttribute('onclick', 'pop(this)');
   newBalloon.style.color = 'hsl(' + colors[Math.floor(Math.random() * colors.length)] + ', .7)';
   newBalloon.style.transitionDuration = speed[Math.floor(Math.random() * speed.length)];
   newBalloon.style.left = Math.floor(Math.random() * (wrapper.offsetWidth - 100)) + 'px';
   wrapper.appendChild(newBalloon);
+  interval = setInterval(removeBalloon, 500, newBalloon);
+}
+
+const removeBalloon = (balloon) => {
+  if (balloon.offsetTop <= -120) {
+    wrapper.removeChild(balloon);
+    missed++;
+  };
 }
 
 function pop(b) {
@@ -36,9 +49,48 @@ function pop(b) {
     sessionStorage.popcount = 1;
   }
   document.getElementById('counter').innerHTML = sessionStorage.popcount;
-  setTimeout(function() {
+  setTimeout(function () {
     b.parentNode.removeChild(b);
   }, 500);
   document.getElementById('pop-counter').style.top = '30px';
   document.getElementById('pop-counter').style.left = '-65px';
 }
+
+let endless;
+const endlessMode = () => {
+  releaseBalloon();
+  endless = setInterval(releaseBalloon, 500);
+}
+
+balloonButton.addEventListener('click', () => {
+  endlessMode();
+  balloonButton.style.display = 'none';
+  title.classList.add('disappear');
+});
+
+const pauseFns = (() => {
+
+  const pauseBtn = document.querySelector('.pause');
+  const pauseBalloons = () => {
+    const balloons = document.querySelectorAll('.balloon');
+    balloons.forEach(item => {
+      item.classList.remove('balloon-rise');
+      item.style.top = getComputedStyle(item).top;
+    })
+  }
+
+  const pauseNewBalloons = () => {
+    clearInterval(endless);
+  }
+
+  const play = () => {
+
+  }
+
+  const pauseGame = () => {
+    pauseBalloons();
+    pauseNewBalloons();
+  }
+  pauseBtn.addEventListener('click', pauseGame);
+
+})()
